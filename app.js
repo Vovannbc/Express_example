620ecb6737ddb28f1712b0fc7dd333fbebd093da
Osnadchuk Vladimir <osnadchuk.vladimir@gmail.com>
2016-12-06 09:09:03 +0200

    trying to make database connect, app doesn't work

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
+var credentials = require('./credentials.js');
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var opts = {
+    server:{
+        socketOptions: {keepAlive: 1}
+    }
+};
+var nodemailer = require('nodemailer');
+var mailTransport = nodemailer.createTransport('SMTP', {
+    service: "Gmail",
+    auth: {
+        user: credentials.gmail.user,
+        pass: credentials.gmail.password
+    }
+});
+
+switch (app.get('env')){
+    case 'development':
+        mongoose.connect(credentials.mongo.development.connectionString, opts);
+        break;
+    case 'production':
+        mongoose.connect(credentials.mongo.production.connectionString, opts);
+        break;
+    default :
+        throw new Error('Неизвестная среда выполнения '+ app.get('env'));
+}
 
 app.set('port', process.env.PORT || 7000);
 app.set('view engine', 'jade');
@@ -8,7 +34,7 @@ app.set('views', __dirname +'/views');
 var database = require('./db.js');
 database.initdb;
 
-var credentials = require('./credentials.js');
+
 app.use(    require('cookie-parser')(credentials.cookieSecret),
             express.static(__dirname + '/public'),
             function(err, req, res, next){
@@ -18,43 +44,34 @@ app.use(    require('cookie-parser')(credentials.cookieSecret),
 app.get('/', function(req, res){
     res.render('home', {title: "Home Page"});
 });
-
 app.get('/aboutus', function(req, res){
     res.render('aboutus');
 });
-
 app.get('/add_receipt', function(req, res){
     res.render('add_receipt');
 });
-
+app.get('/allreceipt', function(req, res){
+    res.render('allreceipts', {TEST: TEST});
+});
 app.get('/allreceipt', function(req, res){
     res.render('allreceipts', {TEST: TEST});
 });
 
-// app.post('/add_receipt', function (req, res, next) {
-//     console.log(req.body);            //{}
-//     console.log(req.body.title);      //undefined
-//     console.log(req.body.link);       //undefined
-//     res.send("Thanks for inserting!");
-//     res.json(req.body);               //{}
-//     next();
-// });
+//!!!!!!!!!here other routes
 
-app.get('/allreceipt', function(req, res){
-    res.render('allreceipts', {TEST: TEST});
-});
 
-app.use(function(req, res){
+
+app.use(
+    function(req, res){
     res.type('text/html');
     res.status(404);
-    res.render('404');
-});
+    res.render('404');},
 
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500);
-    res.render('500');
-});
+    function(err, req, res, next){
+        console.error(err.stack);
+        res.status(500);
+        res.render('500');    }
+);
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to terminate');
