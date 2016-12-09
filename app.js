ab241521a16b09c2b5fba33936f21475997a479d
osnadchuk.vladimir <osnadchuk.vladimir@gmail.com>
2016-12-09 17:26:17 +0200

    realise mail sending from form, app works

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+
 var mongoose = require('mongoose');
 var opts = {
     server:{
@@ -9,13 +10,9 @@ var opts = {
 var credentials = require('./credentials.js');
 
 var nodemailer = require('nodemailer');
-var mailTransport = nodemailer.createTransport('SMTP', {
-    service: "Gmail",
-    auth: {
-        user: credentials.gmail.user,
-        pass: credentials.gmail.password
-    }
-});
+var transporter = nodemailer.createTransport('smtps://'+credentials.gmail.user+':'+credentials.gmail.password+'@smtp.gmail.com');
+
+
 
 // switch (app.get('env')){
 //     case 'development':
@@ -41,6 +38,7 @@ app.use(    require('cookie-parser')(credentials.cookieSecret),
             function(err, req, res, next){
                 console.log('Error : ' + err.message);
                 next()});
+app.use(require('body-parser'). urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
     res.render('home', {title: "Home Page"});
@@ -51,29 +49,58 @@ app.get('/aboutus', function(req, res){
 app.get('/add_receipt', function(req, res){
     res.render('add_receipt', {title: "Add new receipt"});
 });
-app.get('/allreceipt', function(req, res){
+app.get('/allreceipts', function(req, res){
     res.render('allreceipts', {title: "List Of Receipts"});
 });
 
 app.get('/rating', function(req, res){
     res.render('rating', {title: "Rating of receipts"});
 });
+
+app.get('/thank-you', function(req, res){
+    res.render('thank-you');
+});
+
+app.post('/aboutus', function (req, res) {
+    var text = req.body.text;
+    var email = req.body.email;
+    var subject = req.body.subject;
+
+    transporter.sendMail({from: credentials.gmail.name+" "+credentials.gmail.surname+ '<credentials.gmail.user>',
+            to: email,
+            subject: subject,
+            text: text},
+        function(error, info){ if(error){return console.log(error);}
+            console.log('Message sent: ' + info.response);
+        });
+    res.redirect(303, '/thank-you'/*, {message: "Thank you for sending letter!"}*/)
+});
+
+app.post('/add_receipt', function (req, res) {
+    var text = req.body.text;
+    var title = req.body.title;
+    var ingredients = req.body.ingredients;
+    var description = req.body.description;
+    var link = req.body.link;
+
+    res.redirect(303, '/thank-you', {message: "Thank you for new receipt!"})
+});
 //!!!!!!!!!here other routes
 
 
 
-app.use(
-    function(req, res){
+app.use(function(req, res){
     res.type('text/html');
     res.status(404);
-    res.render('404');},
+    res.render('404')});
 
-    function(err, req, res, next){
+app.use(function(err, req, res, next){
         console.error(err.stack);
         res.status(500);
-        res.render('500');    }
-);
+        res.render('500')});
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + ' press Ctrl-C to terminate');
 });
+
+module.exports = app;
